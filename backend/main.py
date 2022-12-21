a9142d906039f6e726cdd71a3ca82a76029aff03
bitesnail <rkwoal216@gmail.com>
2022-12-22 03:19:14 +0900

    Add CORS Setting

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.exchange import exchange_url
-from src.database import db, reverse_db
+from src.database import reverse_db
+from src.itemclass import ORIGNITEM
 import uvicorn
 
 app = FastAPI()
+
+origins = {
+    "http://localhost",
+    "http://localhost:3000",
+}
+
+app.add_middleware(
+   CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials =True,
+    allow_methods = ["*"],
+    allow_headers= ["*"],
+)
 
 @app.get('/{hashed_id}')
 async def link_to_site(hashed_id: str):
@@ -13,8 +28,8 @@ async def link_to_site(hashed_id: str):
     return {"origin_url" : "error"}
 
 @app.post('/exchange')
-async def exchange(originItem):
+async def exchange(originItem:ORIGNITEM):
     return exchange_url(originItem)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
