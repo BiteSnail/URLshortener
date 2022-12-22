b16c7bd2cca7e7d555ec2666b79d6cbfe11f6152
bitesnail <rkwoal216@gmail.com>
2022-12-22 16:25:55 +0900

    Add database connection

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,9 +1,21 @@
-db = {}
-reverse_db = {}
+from pymongo import MongoClient
+
+hostname = 'localhost'
+portnum = 27017
+databasename = 'test'
+collectionname = 'urlitem'
+
+Client = MongoClient(host=hostname, port=portnum)
+db = Client[databasename]
+items = db[collectionname]
 
 def insert_item_to_db(origin, shorten):
-    db[origin] = shorten
-    reverse_db[shorten[:5]] = origin
+    item = {"origin": origin,
+            "shorten": shorten}
+    item_id = items.insert_one(item).inserted_id
 
-def is_in_db(origin):
-    return origin in db
+def find_in_db(origin):
+    return items.find_one({"origin":origin})
+
+def link_in_db(shorten_id):
+    return items.find_one({"shorten":shorten_id})['origin']
--- backend/src/exchange.py
+++ backend/src/exchange.py
@@ -1,15 +1,16 @@
-from src.database import db, is_in_db, insert_item_to_db
+from src.database import find_in_db, insert_item_to_db
 from src.itemclass import make_hashable_url, ORIGNITEM
 
 site_domain = "http://localhost:3000/"
 
 def exchange_url(originItem:ORIGNITEM):
     #TODO : find originItem in DB
+    item = find_in_db(originItem.origin_url)
 
-    if is_in_db(originItem.origin_url):
-        shorten_url = db[originItem.origin_url]
+    if item == None:
+        shorten_id = make_hashable_url(originItem.origin_url)
+        insert_item_to_db(originItem.origin_url, shorten_id)
     else:
-        shorten_url = make_hashable_url(originItem.origin_url)
-        insert_item_to_db(originItem.origin_url, shorten_url)
+        shorten_id = item['shorten']
     
-    return {"shorten_url" : site_domain+shorten_url}
+    return {"shorten_url" : site_domain+shorten_id}
