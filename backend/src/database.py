071918dd11d7e3ceac80d20678fa53d4d21eda57
bitesnail <rkwoal216@gmail.com>
2022-12-23 15:28:46 +0900

    Add server cache class

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,11 +2,32 @@ from pymongo import MongoClient
 from src.setconfig import get_config
 import configparser
 
+class Cache:
+    def __init__(self, config:configparser.ConfigParser):
+        self._storage = {}
+        self.capacity = int(config['capacity'])
+        self.size = int(config['size'])
+    
+    def find_item(self, shorten_id:str):
+        if shorten_id in self._storage:
+            return {'origin': self._storage[shorten_id],
+                    'shorten': shorten_id}
+        else:
+            return None
+    
+    def insert_item(self, item:dict):
+        if self.size < self.capacity:
+            self._storage.update(item)
+        else:
+            self._storage.popitem()
+            self._storage.update(item)
+        print(self._storage)
+
 class DataBase:
     def __init__(self, config:configparser.ConfigParser):
         self.Client = MongoClient(host=config['host'], port=int(config['port']))
         self.items = self.Client[config['database']][config['collection']]
-        self.cache = {}
+        self.cache = Cache(get_config('cache'))
     
     def insert_item_to_db(self, origin:str, shorten:str):
         item = {"origin": origin,
--- backend/src/exchange.py
+++ backend/src/exchange.py
@@ -6,10 +6,11 @@ site_domain = "http://localhost:3000/"
 def exchange_url(originItem:ORIGNITEM):
     shorten_id = Hash_url.make_hashable_url(originItem.origin_url)
 
-    item = db.find_in_cache(shorten_id)
+    item = db.cache.find_item(shorten_id)
     if not item == None:
         return {"shorten_url" : site_domain+shorten_id}
 
+    db.cache.insert_item({shorten_id:originItem.origin_url})
     item = db.find_in_db(key='shorten', value=shorten_id)
     if item == None:
         db.insert_item_to_db(originItem.origin_url, shorten_id)
