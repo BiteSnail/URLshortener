32ac5fb8423cc0ecf9ca228cdfd170d7abaed4ce
bitesnail <rkwoal216@gmail.com>
2022-12-23 17:16:19 +0900

    Add database connection exception

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pymongo
 from src.setconfig import get_config
 import configparser
 
@@ -8,7 +9,7 @@ class Cache:
         self.capacity = int(config['capacity'])
         self.size = int(config['size'])
     
-    def find_item(self, shorten_id:str):
+    def find_item(self, shorten_id:str) -> dict | None:
         if shorten_id in self._storage:
             return {'origin': self._storage[shorten_id],
                     'shorten': shorten_id}
@@ -21,26 +22,43 @@ class Cache:
         else:
             self._storage.popitem()
             self._storage.update(item)
-        print(self._storage)
-
 class DataBase:
     def __init__(self, config:configparser.ConfigParser):
-        self.Client = MongoClient(host=config['host'], port=int(config['port']))
+        try:
+            self.Client = MongoClient(host=config['host'], 
+                                    port=int(config['port']), 
+                                    serverSelectionTimeoutMS=int(config['maxSevSelDelay']))
+            self.Client.server_info()
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            print(err)
+            exit()
+
         self.items = self.Client[config['database']][config['collection']]
         self.cache = Cache(get_config('cache'))
     
     def insert_item_to_db(self, origin:str, shorten:str):
         item = {"origin": origin,
                 "shorten": shorten}
-        item_id = self.items.insert_one(item).inserted_id
+        try:
+            item_id = self.items.insert_one(item).inserted_id
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            print(err)
 
-    def find_in_db(self, key:str, value:str):
-        return self.items.find_one({key:value})
+    def find_in_db(self, key:str, value:str)-> dict | None:
+        try:
+            return self.items.find_one({key:value})
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            print(err)
+            return None
 
-    def link_in_db(self, shorten_id:str):
-        return self.items.find_one({"shorten":shorten_id})['origin']
+    def link_in_db(self, shorten_id:str)-> dict | None:
+        try:
+            return self.items.find_one({"shorten":shorten_id})['origin']
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            print(err)
+            return None
     
-    def find_in_cache(self, shorten_id: str):
+    def find_in_cache(self, shorten_id: str)-> dict | None:
         if shorten_id in self.cache:
             return {"origin": self.cache[shorten_id],
                     "shorten": shorten_id}
