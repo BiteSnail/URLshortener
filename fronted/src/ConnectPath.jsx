c3108d8333b894e4cec73962e298f10ea5229ee0
bitesnail <rkwoal216@gmail.com>
2022-12-24 17:22:45 +0900

    Add exception

--- fronted/src/ConnectPath.jsx
+++ fronted/src/ConnectPath.jsx
@@ -1,15 +1,43 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios"
+import "./MainPage.Module.scss"
 
 export const ConnectPath = () => {
     const params = useParams();
-
+    const [page, setPage] = useState(
+    <div>
+        <p>사이트로 이동 중입니다.</p>
+    </div>)
     axios
         .get("http://localhost:8000/"+params.id,{
         })
         .then(function (response) {
-            const url = response.data.origin_url
-            window.location.href = url;
+            if(response.data.origin_url === "error"){
+                setPage(
+                    <div className="main-body">
+                        <div>
+                            <header>
+                                <nav className="main-title">
+                                    <a href="/" className="home-link">Page Not found</a>
+                                    <div>
+
+                                    </div>
+                                </nav>
+                            </header>
+                        </div>
+                        <footer>
+                            <div>
+                                <h3>INFO</h3>
+                                <p>Email : rkwoal216@gmail.com</p>
+                            </div>
+                        </footer>
+                    </div>
+                )
+            }
+            else{
+                window.location.href = response.data.origin_url;
+            }
         })
         .catch(function (error) {
             return (
@@ -21,9 +49,7 @@ export const ConnectPath = () => {
         })
     
     return (
-        <div>
-            <p>사이트로 이동 중입니다.</p>
-        </div>
+        page
     )
     
 }
